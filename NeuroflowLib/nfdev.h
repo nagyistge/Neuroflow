22b927a859e2d60cae047c73676c8e92dfc8e157
unbornchikken <gabor.mezo@outlook.com>
2013-12-31 15:09:24 +0100

    untracked files added

---
+++ NeuroflowLib/nfdev.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <sstream>
+#include <exception>
+#include <string>
+#include <tuple>
+#include <functional>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <ppltasks.h>
+#include <random>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/algorithm/string/find.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include "nf_object.h"
+#include "typedefs.h"
+#include "error.h"
+#include "version.h"
+#include "nf_helpers.h"
+
+#define null nullptr
+#define USING using namespace std; using namespace nf; using namespace cl; using namespace concurrency;
