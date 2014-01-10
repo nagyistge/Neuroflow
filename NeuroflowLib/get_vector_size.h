c0f66f1196afa7ae491f35d0212c4af332f24857
unbornchikken <gabor.mezo@outlook.com>
2014-01-10 13:47:15 +0100

    cpplinq nuget

--- NeuroflowLib/get_vector_size.h
+++ NeuroflowLib/get_vector_size.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <algorithm>
-
 namespace nf
 {
     inline idx_t get_vector_size(idx_t size)
@@ -26,27 +24,27 @@ namespace nf
     {
         idx_t s1 = get_vector_size(size1, size2);
         idx_t s2 = get_vector_size(size2, size3);
-        return min(s1, s2);
+        return s1 < s2 ? s1 : s2;
     }
 
     inline idx_t get_vector_size(idx_t size1, idx_t size2, idx_t size3, idx_t size4)
     {
         idx_t s1 = get_vector_size(size1, size2, size3);
         idx_t s2 = get_vector_size(size3, size4);
-        return min(s1, s2);
+        return s1 < s2 ? s1 : s2;
     }
 
     inline idx_t get_vector_size(idx_t size1, idx_t size2, idx_t size3, idx_t size4, idx_t size5)
     {
         idx_t s1 = get_vector_size(size1, size2, size3, size4);
         idx_t s2 = get_vector_size(size4, size5);
-        return min(s1, s2);
+        return s1 < s2 ? s1 : s2;
     }
 
     inline idx_t get_vector_size(idx_t size1, idx_t size2, idx_t size3, idx_t size4, idx_t size5, idx_t size6)
     {
         idx_t s1 = get_vector_size(size1, size2, size3, size4, size5);
         idx_t s2 = get_vector_size(size5, size6);
-        return min(s1, s2);
+        return s1 < s2 ? s1 : s2;
     }
 }
