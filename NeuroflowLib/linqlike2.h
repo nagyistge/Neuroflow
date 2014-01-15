be8de191424909eca72dab396c582cbb9b228c74
unbornchikken <gabor.mezo@outlook.com>
2014-01-15 13:48:46 +0100

    const needed

--- NeuroflowLib/linqlike2.h
+++ NeuroflowLib/linqlike2.h
@@ -2,8 +2,10 @@
 
 #include <boost/coroutine/all.hpp>
 #include <boost/iterator.hpp>
+#include <boost/optional.hpp>
 #include <type_traits>
 #include <functional>
+#include <iterator>
 
 namespace linqlike2
 {
@@ -14,13 +16,53 @@ namespace linqlike2
         typedef typename coro_t::pull_type pull_type;
         typedef typename coro_t::push_type push_type;
         typedef std::function<pull_type()> pull_factory_t;
+        typedef typename boost::range_iterator<pull_type>::type pull_iterator_t;
+        typedef typename std::iterator_traits<pull_iterator_t>::value_type pull_iterator_value_t;
+
+        struct enumerable_iterator : public std::iterator<std::input_iterator_tag, pull_iterator_value_t>
+        {
+            enumerable_iterator() { }
+
+            explicit enumerable_iterator(pull_type&& pull) :
+            _pull(std::move(pull)),
+            _it(boost::begin(_pull))
+            {
+            };
+
+            enumerable_iterator(const enumerable_iterator& mit) : _it(mit._it) { }
+            enumerable_iterator operator=(const enumerable_iterator& mit)
+            {
+                return enumerable_iterator(*this);
+            }
+
+            enumerable_iterator& operator++() { ++_it; return *this; }
+            enumerable_iterator operator++(int) { enumerable_iterator tmp(*this); operator++(); return tmp; }
+            bool operator==(const enumerable_iterator& rhs) { return _it == rhs._it; }
+            bool operator!=(const enumerable_iterator& rhs) { return _it != rhs._it; }
+            pull_iterator_value_t& operator*() { return *_it; }
+
+        private:
+
+            pull_type _pull;
+            pull_iterator_t _it;
+        };;
 
         explicit enumerable(pull_factory_t&& pullFactory) :
             _pullFactory(std::move(pullFactory))
         {
         }
 
-        pull_type run() const
+        enumerable_iterator begin()
+        {
+            return enumerable_iterator(_pullFactory());
+        }
+
+        enumerable_iterator end()
+        {
+            return enumerable_iterator();
+        }
+
+        /*pull_type run() const
         {
             return _pullFactory();
         }
@@ -28,7 +70,7 @@ namespace linqlike2
         pull_type operator*() const
         {
             return run();
-        }
+        }*/
 
     private:
         pull_factory_t _pullFactory;
