93a82e92918637762a8b03549b6d425a16b8de96
unbornchikken <gabor.mezo@outlook.com>
2014-01-16 09:30:24 +0100

    fixed

---
+++ NeuroflowLib/linqlike.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "linqlike_base.h"
+#include "linqlike_from.h"
+#include "linqlike_where.h"
+#include "linqlike_select.h"
--- NeuroflowLib/version.cpp
+++ NeuroflowLib/version.cpp
@@ -3,8 +3,8 @@
 
 #define MAJOR_VER 1
 #define MINOR_VER 0
-#define BUILD_VER 379
-#define REVISION_VER 39309
+#define BUILD_VER 380
+#define REVISION_VER 16928
 
 std::wstring nf::version()
 {
