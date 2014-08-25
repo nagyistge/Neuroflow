cf60fa86afcaf71f12d7b1c700f3b3cad81af484
unbornchikken <gabor.mezo@outlook.com>
2014-08-25 08:38:12 +0200

    vault has been compressed

--- _vault/NFBak/Neuroflow_vsonline/Neuroflow/OldStuff/NeoComp/NeoComp 1.0/NeoComp.Core/SelectionAlgorithms/SelectionDirection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NeoComp.SelectionAlgorithms
-{
-    public enum SelectionDirection : byte { FromTop, FromBottom }
-}
--- _vault/NFBak/Neuroflow_vsonline/Neuroflow/OldStuff/Neuroflow Framework/Neuroflow/Core/NeuralNetworks/Learning/Algorithms/LearningMode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Neuroflow.Core.NeuralNetworks.Learning.Algorithms
-{
-    public enum LearningMode : byte { Stochastic = 0, Batch = 1 }
-}
