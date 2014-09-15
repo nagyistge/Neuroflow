b7fdcb7d31943b290dd197d09b04c6d676690c89
unbornchikken <gabor.mezo@outlook.com>
2014-09-15 20:12:51 +0200

    learning impl in progress

--- Dlang/neuroflow/nf_core/layer.d
+++ Dlang/neuroflow/nf_core/layer.d
@@ -1,26 +1,31 @@
 ﻿import layerbehavior;
 import layerdescription;
+import layerconnections;
+import std.container;
 
 class Layer
 {
-    this(size_t size, in immutable(Object)[] args ...)
+    this(size_t size, in Object[] args ...)
     {
         _size = size;
         foreach (arg; args)
         {
-            auto b = cast(immutable(LayerBehavior))arg;
+            auto b = cast(LayerBehavior)arg;
             if (b)
             {
                 _behaviors ~= b;
                 continue;
             }
-            auto d = cast(immutable(LayerDescription))arg;
+            auto d = cast(LayerDescription)arg;
             if (d)
             {
                 _descriptions ~= d;
                 continue;
             }
         }
+
+        _inputConnections = new LayerConnections(LayerConnections.ConnectionType.input, this);        
+        _outputConnections = new LayerConnections(LayerConnections.ConnectionType.output, this);
     }
 
     @property size_t size() const
@@ -28,19 +33,33 @@ class Layer
         return _size;
     }
 
-    @property immutable(LayerBehavior)[] behaviors()
+    @property ref Array!LayerBehavior behaviors()
     {
         return _behaviors;
     }
 
-    @property immutable(LayerDescription)[] desciptions()
+    @property ref Array!LayerDescription desciptions()
     {
         return _descriptions;
     }
 
+    @property LayerConnections inputConnections()
+    {
+        return _inputConnections;
+    }
+
+    @property LayerConnections outputConnections()
+    {
+        return _outputConnections;
+    }
+
     private size_t _size;
 
-    private immutable(LayerBehavior)[] _behaviors;
+    private Array!LayerBehavior _behaviors;
 
-    private immutable(LayerDescription)[] _descriptions;
+    private Array!LayerDescription _descriptions;
+
+    private LayerConnections _inputConnections;
+
+    private LayerConnections _outputConnections;
 }
--- Dlang/neuroflow/nf_core/layerconnections.d
+++ Dlang/neuroflow/nf_core/layerconnections.d
@@ -11,13 +11,21 @@ enum FlowDirection
 
 class LayerConnections
 {
-    private enum ConnectionType { input, output };
+    enum ConnectionType { input, output };
 
     private struct OtherLayer
     {
         FlowDirection flowDirection;
 
         Layer layer;
+    }
+
+    this(ConnectionType type, Layer connectedLayer)
+    {
+        assert(connectedLayer);
+
+        _type = type;
+        _connectedLayer = connectedLayer;
     }
 
     void addOneWay(Layer layer)
@@ -42,7 +50,6 @@ class LayerConnections
         {
             suppressed(
             layer, 
-            () =>
             {
                 if (_type == ConnectionType.output)
                 {
@@ -52,22 +59,56 @@ class LayerConnections
                 {
                     layer.outputConnections.add(_connectedLayer, direction);
                 }
-            });            
+            }); 
+            _otherLayers ~= OtherLayer(direction, layer);
         }
     }
 
     bool remove(Layer layer)
     {
         enforce(layer, "Layer is null.");
+
+        auto len = _otherLayers.length;
+        _otherLayers = .remove!(ol => ol.layer == layer)(_otherLayers);
+        if (len != _otherLayers.length)
+        {
+            assert(_otherLayers.length + 1 == len);
+            suppressed(
+            layer, 
+            {
+                if (_type == ConnectionType.output)
+                {
+                    layer.inputConnections.remove(_connectedLayer);
+                }
+                else
+                {
+                    layer.outputConnections.remove(_connectedLayer);
+                }
+            }); 
+            return true;
+        }
+        return false;
     }
 
-    void clear();
-
-    LayerConnections connectedLayers(FlowDirection direction) const;
-    
-    private this(ConnectionType type)
+    void clear()
     {
-        _type = type;
+        foreach (ol;_otherLayers)
+        {
+            if (_type == ConnectionType.output)
+            {
+                ol.layer.inputConnections.remove(_connectedLayer);
+            }
+            else
+            {
+                ol.layer.outputConnections.remove(_connectedLayer);
+            }
+        }
+        _otherLayers.length = 0;
+    }
+
+    auto connectedLayers(FlowDirection direction) const
+    {
+        return _otherLayers.filter!(ol => ol.flowDirection == direction).map!(ol => ol.layer);
     }
     
     private OtherLayer[] _otherLayers;
@@ -77,19 +118,22 @@ class LayerConnections
     private ConnectionType _type;
 
     private Layer _connectedLayer;
-    
-    private void init(Layer connectedLayer)
+
+    private void suppressed(Layer layer, void delegate() method)
     {
-        assert(mustInit);
-        assert(connectedLayer);
-
-        _connectedLayer = connectedLayer;
+        if (!_suppressOtherSideUpdate)
+        {
+            layer.inputConnections._suppressOtherSideUpdate = true;
+            layer.inputConnections._suppressOtherSideUpdate = true;
+            try
+            {                
+                method();
+            }
+            finally
+            {
+                layer.inputConnections._suppressOtherSideUpdate = false;
+                layer.inputConnections._suppressOtherSideUpdate = false;
+            }
+        }
     }
-
-    private bool mustInit() const
-    {
-        return _connectedLayer is null;
-    }
-
-    private void suppressed(Layer layer, in void delegate() method);
 }
