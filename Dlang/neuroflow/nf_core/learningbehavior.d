dc63efbfe5edddd864d199eedc42c23736cb2ce0
unbornchikken <gabor.mezo@outlook.com>
2014-09-15 20:12:51 +0200

    learning impl in progress

---
+++ Dlang/neuroflow/nf_core/learningbehavior.d
@@ -0,0 +1,5 @@
+﻿import layerbehavior;
+
+interface LearningBehavior : LayerBehavior
+{
+}
---
+++ Dlang/neuroflow/nf_core/supervisedlearningbehavior.d
@@ -0,0 +1,21 @@
+﻿import learningbehavior;
+
+enum SupervisedWeightUpdateMode
+{
+    offline,
+    online
+}
+
+enum SupervisedOptimizationType
+{
+    none,
+    gradientBased,
+    global
+}
+
+class SupervisedLearningBehavior : LearningBehavior
+{
+    abstract SupervisedOptimizationType optimizationType() const;
+
+    abstract SupervisedWeightUpdateMode weightUpdateMode() const;
+}
